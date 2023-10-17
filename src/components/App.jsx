f54c3b16dba7d341fd00b1c336d0afaa9b94a4b8
ArtSatanov <artsatanov@gmail.com>
2023-10-18 02:10:29 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,18 @@
-export const App = () => {
-  return <div></div>;
-};
+import { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+
+export class App extends Component {
+  state = {};
+
+  componentDidMount() {}
+
+  componentDidUpdate() {}
+
+  render() {
+    return (
+      <div>
+        <Searchbar />
+      </div>
+    );
+  }
+}
