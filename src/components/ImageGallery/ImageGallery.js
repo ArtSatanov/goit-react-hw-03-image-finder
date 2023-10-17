067ab550fc961fb7622efce00c75bcee905867c7
ArtSatanov <artsatanov@gmail.com>
2023-10-18 02:22:08 +0300

    1

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,3 +1,15 @@
-export const ImageGallery = () => {
-  return <ul class="gallery"></ul>;
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+
+export const ImageGallery = ({ arrayResponse }) => {
+  return (
+    <ul class="gallery">
+      {arrayResponse.map(elem => {
+        <ImageGalleryItem
+          key={elem.id}
+          smallImg={elem.webformatURL}
+          largeImg={elem.largeImageURL}
+        />;
+      })}
+    </ul>
+  );
 };
--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,7 +1,7 @@
-export const ImageGalleryItem = () => {
+export const ImageGalleryItem = ({ webformatURL, largeImageURL }) => {
   return (
     <li class="gallery-item">
-      <img src="" alt="" />
+      <img src={webformatURL} alt="" />
     </li>
   );
 };
