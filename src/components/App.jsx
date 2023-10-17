cdfbd88c2a36ff8b715fdce945154a75d52ae667
ArtSatanov <artsatanov@gmail.com>
2023-10-18 02:41:04 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,9 @@ import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 
 export class App extends Component {
-  state = {};
+  state = {
+    filter: '',
+  };
 
   componentDidMount() {}
 
